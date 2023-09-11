caf42a2aca74372043dcd9d72e6e778d1d7fc754
Bruno Campos <campos.bruno98@gmail.com>
2023-09-11 18:18:54 -0300

    configuring scss

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,8 +7,10 @@ export default function Header() {
     <header>
       <section className={styles.container}>
         <div className={styles.subGroup}>
-          <img src="" alt="" />
-          <p>Compra 100% segura</p>
+          <img src="icons/shield.svg" alt="" />
+          <p>
+            Compra <strong className={styles.pinkText}>100% segura</strong>
+          </p>
         </div>
         <div className={styles.subGroup}>
           <Image className={styles.truck} src="icons/truck.svg" alt="" width="30" height="30" />
@@ -17,8 +19,10 @@ export default function Header() {
           </p>
         </div>
         <div className={styles.subGroup}>
-          <img src="" alt="" />
-          <p>Parcele suas compras</p>
+          <img src="icons/card.svg" alt="" />
+          <p>
+            <strong className={styles.pinkText}>Parcele</strong> suas compras
+          </p>
         </div>
       </section>
       <section></section>
