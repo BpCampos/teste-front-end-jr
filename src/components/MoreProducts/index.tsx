6dcab78c199507f90fb41a4178df90c85703da89
Bruno Campos <campos.bruno98@gmail.com>
2023-09-13 11:28:29 -0300

    finishing the second products section

--- src/components/MoreProducts/index.tsx
+++ src/components/MoreProducts/index.tsx
@@ -10,6 +10,24 @@ export default function MoreProducts() {
           <p>Ver todos</p>
         </div>
       </section>
+      <section className={styles.products__banners}>
+        <div>
+          <img src="/images/image 101.png" alt="" />
+          <article>
+            <p>Produtos</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <button>CONFIRA</button>
+          </article>
+        </div>
+        <div>
+          <img src="/images/image 101.png" alt="" />
+          <article>
+            <p>Produtos</p>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <button>CONFIRA</button>
+          </article>
+        </div>
+      </section>
     </div>
   )
 }
